50696d7c73134000fc883f8c7e8dee628ab00765
aysenurkocak <38115567+aysenurkocak@users.noreply.github.com>
2019-06-08 07:21:43 +0300

    Add files via upload

--- Builder-Afactory/Builder.cs
+++ Builder-Afactory/Builder.cs
@@ -1,42 +1,83 @@
-        private void AnaForm_Load(object sender, EventArgs e)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    //Product Class
+    class Faturalar
+    {
+        public string FaturaNo { get; set; }
+        public string Lokaasyon { get; set; }
+        public string Tarih { get; set; }
+        public string FirmaAdi { get; set; }
+        public double Tutar { get; set; }
+
+
+    }
+    //Builder
+    abstract class IFaturaBuilder
+    {
+        protected Faturalar fatura;
+        public Faturalar Fatura
         {
-            ////Proje Ýçin
-            ReportInfo info = new ReportInfo();
-            info.Title = "Gider Grafiði";
-            
-            info.Expenses = new List<string>();
-
-            //Faturalarýn toplam tutarlarý veritabanýnda bir SP den liste halide geliyor biz bunlarý grafik ile gösteriyoruz.
-            AsyaDbEntities DbIslemlerim = new AsyaDbEntities();
-            List<S_Grafik_Result> GrafikList = new List<S_Grafik_Result>();
-            GrafikList  = DbIslemlerim.S_Grafik().ToList();
-            decimal sayac = 0;
-            foreach(S_Grafik_Result x in GrafikList)
-            {
-                if(x.Ucret != null)
-                {
-                    //Hangi fatura ve toplam tutarý ne !!!
-                    info.Expenses.Add(x.Adi + " (" + Decimal.Round(Convert.ToDecimal(x.Ucret), 2) + ")", Decimal.Round(Convert.ToDecimal(x.Ucret), 2));
-                    sayac = sayac + Decimal.Round(Convert.ToDecimal(x.Ucret),2);
-                    chart1.Series["Series1"].Points.AddXY(
-                    x.Adi + " (" + Decimal.Round(Convert.ToDecimal(x.Ucret), 2) + ")", Decimal.Round(Convert.ToDecimal(x.Ucret), 2) 
-                    );
-                }
-               
-            }
-
-            if (sayac <= 0)
-                lblGrafik.Visible = false;
-            else
-            {
-                lblGrafik.Visible = true;
-                lblGrafik.Text = "Kayýtlý Fatura  Ücreti Toplamý : " + sayac;
-                info.TotalCost = sayac;
-            }
-            
-            ReportBuilderBase builder = new DivBasedReportBuilder(info);
-            ReportManager reportManager = new ReportManager(builder);
-
-            string str = reportManager.Build();
-           
+            get { return fatura; }
         }
+
+        public abstract void SetFaturaNo();
+        public abstract void SetLokaasyon();
+        public abstract void SetTarih();
+        public abstract void SetFirmaAdi();
+        public abstract void SetTutar();
+    }
+
+    //ConcreteBuilder Class
+    class KiraConcreteBuilder : IFaturaBuilder
+    {
+        public KiraConcreteBuilder()
+        {
+           fatura = new Faturalar();
+        }
+        public override void SetTarih() => Fatura.Tarih = "12.02.2019";
+        public override void SetFaturaNo() => Fatura.FaturaNo = "12233ADHHF";
+        public override void SetLokaasyon() => Fatura.Lokaasyon = "Beylükdüzü";
+        public override void SetFirmaAdi() => Fatura.FirmaAdi = "Koç";
+        public override void SetTutar() => Fatura.Tutar = 12.000;
+    }
+    //ConcreteBuilder Class
+    class ÝletisimConcreteBuilder : IFaturaBuilder
+    {
+        public ÝletisimConcreteBuilder()
+        {
+            fatura = new Faturalar();
+        }
+        public override void SetTarih() => Fatura.Tarih = "12.02.2019";
+        public override void SetFaturaNo() => Fatura.FaturaNo = "ytyuHF";
+        public override void SetLokaasyon() => Fatura.Lokaasyon = "Maltepe";
+        public override void SetFirmaAdi() => Fatura.FirmaAdi = "Aysur";
+        public override void SetTutar() => Fatura.Tutar = 16.000;
+    }
+    //Director Class
+    class FaturaUret
+    {
+        public void Uret(IFaturaBuilder Fatura)
+        {
+            Fatura.SetTutar();
+            Fatura.SetFaturaNo();
+            Fatura.SetLokaasyon();
+            Fatura.SetFirmaAdi();
+            Fatura.SetTarih();
+        }
+    }
+}
+
+
+//BUÝLDER PATTERN KULLANIMI
+
+//IFaturaBuilder fatura = new KiraConcreteBuilder();
+//FaturaUret uret = new FaturaUret();
+//uret.Uret(fatura);
+//fatura = new ÝletisimConcreteBuilder();
