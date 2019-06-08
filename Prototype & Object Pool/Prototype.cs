83f8ea78eb5ee72a0b1abdada3b0d061071a9ec9
aysenurkocak <38115567+aysenurkocak@users.noreply.github.com>
2019-06-08 07:22:32 +0300

    Add files via upload

---
+++ Prototype & Object Pool/Prototype.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+   
+    abstract class PrototypeOdeme
+    {
+        public abstract PrototypeOdeme Clone();
+    }
+
+
+    class Odeme : PrototypeOdeme
+    {
+        public int OdemeID { get; set; }
+        public string OdemeAdi { get; set; }
+        public string OdemeTuru { get; set; }
+        public bool Durum { get; set; }
+
+        //durum ödendi bilgisi
+        public Odeme(int OdemeID, string OdemeAdi, string OdemeTuru, bool Durum)
+        {
+            this.OdemeID = OdemeID;
+            this.OdemeAdi = OdemeAdi;
+            this.OdemeTuru = OdemeTuru;
+            this.Durum = Durum;
+        }
+
+        public override PrototypeOdeme Clone()
+        {
+            return this.MemberwiseClone() as PrototypeOdeme;
+        }
+    }
+
+}
